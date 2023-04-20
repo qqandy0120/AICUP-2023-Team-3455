3758ad061fe6a48c210f45e43c9d175f3ea20607
b09902141@ntu.edu.tw <yd3214813@gmail.com>
2023-04-20 23:07:16 +0800

    update test_openai with prefix.txt

--- test_openai.py
+++ test_openai.py
@@ -1,20 +1,19 @@
 import openai
 import json
+import os
 openai.api_key = "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" #google account
 # openai.api_key = "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" #csie account
 model_engine = "gpt-3.5-turbo-0301"
 
-prompt_prefix =' \
-    給定一個dictionary，其中包含claim以及數個candidates，claim為一個錯誤或正確的敘述，candidates為數篇我們查到的相關文章，每個candidates由數組{篇名 : {句子id : 句子}} 組成。\n \
-    請考慮所有candidates，以中文選擇claim為正確或錯誤，並選出支持的句子作為證據，句子以 "篇名, 句子id" 表示，不需說明理由。\n'
-
+with open('prefix.txt', 'r') as f:
+    prompt_prefix = f.read()
 
 def get_response(prompt):
     # Get the response from GPT-3.5
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
-            {"role": "user", "content": prompt}
+            {"role": "user", "content" : prompt}
         ]
     )
     # Extract the response from the response object
@@ -28,19 +27,28 @@ def main():
     with open('./cache/train_sentence_concat.json', 'r') as f:
         claim_candidates = json.load(f)
 
+    version = 3
     # record prompt prefix
-    with open(f'./cache/response_v1/prefix.json', 'w') as f:
-        json.dump(prompt_prefix, f, indent=2, ensure_ascii=False)
+    try:
+        os.mkdir(f'./cache/response_v{version}')
+    except:
+        print('the dir exists')
+        exit(1)
+
+    with open(f'./cache/response_v{version}/prefix.json', 'w', encoding='utf-8') as f:
+        f.write(prompt_prefix)
     
     # choose candidates
-    for i in range(3):
+    id_list = [0, 1, 5, 6, 7]
+    for i in id_list:
         candidate = claim_candidates[i]
         # concat with the prompt
         user_input = prompt_prefix + f'{candidate}'
         
         chatbot_response = get_response(user_input)
-        with open(f'./cache/response_v1/response{i}.json', 'w') as f:
+        chatbot_response.update({'prompt' : user_input})
+        with open(f'./cache/response_v{version}/response{i}.json', 'w') as f:
             json.dump(chatbot_response, f, indent=2, ensure_ascii=False)
-        print(f'finished generated prompt {i}')
+        print(f'finished generated response of training data_{i}')
 
 main()
