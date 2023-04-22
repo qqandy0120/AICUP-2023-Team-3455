eb1b556ec6860dd5a77eb549f3bb87fe700c6d0a
b09902141@ntu.edu.tw <yd3214813@gmail.com>
2023-04-22 15:34:38 +0800

    version 6 of test_openai

--- generate_prompt.py
+++ generate_prompt.py
@@ -17,7 +17,7 @@ from pandarallel import pandarallel
 # our own libs
 from utils import load_json
 
-pandarallel.initialize(progress_bar=True, verbose=0, nb_workers=4)
+pandarallel.initialize(progress_bar=True, verbose=0, nb_workers=6)
 wikipedia.set_lang("zh")
 
 from utils import (
@@ -29,10 +29,18 @@ from utils import (
     set_lr_scheduler,
 )
 
+Path('cache/').mkdir(parents=True, exist_ok=True)
 wiki_pages = jsonl_dir_to_df("data/wiki-pages")
 mapping = generate_evidence_to_wiki_pages_mapping(wiki_pages)
 del wiki_pages
-print(mapping['天王星'])
+# print(mapping['天王星'])
+''' (some empty string)
+{'0': '天王星 （ Uranus ） 是一顆在太陽系中離太陽第七近的青色行星 ， 其體積在太陽系中排名第三 、 質量排名第四 。', 
+ '1': '', 
+ '2': '',
+ '3': '天王星的英文名稱Uranus來自古希臘神話的天空之神烏拉諾斯  ， 是克洛諾斯的父親 、 宙斯的祖父 。', 
+ '4': '在西方文化中 ， 天王星是太陽系中唯一以希臘神祇命名的行星 ， 其他行星都依照羅馬神祇命名 。', 
+'''
 
 with open('./cache/train_doc5.json', 'r') as f:
     training_set = json.load(f)
@@ -42,7 +50,7 @@ with open('./cache/train_sentence_concat.json', 'w') as f_out:
     for data in training_set:
         claim_and_candidates = {
             'claim' : data['claim'],
-            'candidates' : {},
+            'data' : {},
             }
         for evi in data['predicted_pages']:
             try:
@@ -50,7 +58,7 @@ with open('./cache/train_sentence_concat.json', 'w') as f_out:
             except:
                 continue
             evi_mapping = {f'{evi}' : {id : mapping[evi][id] for id in mapping[evi] if mapping[evi][id]}}
-            claim_and_candidates['candidates'].update(evi_mapping)
+            claim_and_candidates['data'].update(evi_mapping)
         train_json_list.append(claim_and_candidates)
     json.dump(train_json_list, f_out, indent=2, ensure_ascii=False)
 
--- test_openai.py
+++ test_openai.py
@@ -1,6 +1,8 @@
 import openai
 import json
 import os
+from argparse import ArgumentParser, Namespace
+from pathlib import Path
 openai.api_key = "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" #google account
 # openai.api_key = "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" #csie account
 model_engine = "gpt-3.5-turbo-0301"
@@ -9,48 +11,90 @@ with open('prefix.txt', 'r') as f:
     prompt_prefix = f.read()
 
 def get_response(prompt):
-    # Get the response from GPT-3.5
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "user", "content" : prompt}
         ]
     )
-    # Extract the response from the response object
     return response
 
 
-def main():
+def main(args):
     print(f'[INFO] using model {model_engine}')
     
-    # load candidates
-    with open('./cache/train_sentence_concat.json', 'r') as f:
+    # load preprocessed data
+    with open(args.preprocess_data, 'r') as f:
         claim_candidates = json.load(f)
-
-    version = 5
+    
     # record prompt prefix
     try:
-        os.mkdir(f'./cache/response_v{version}')
+        os.mkdir(f'./cache/response_v{args.version}_{args.version_name}')
     except:
         print('the dir exists')
         exit(1)
 
-    with open(f'./cache/response_v{version}/prefix.txt', 'w', encoding='utf-8') as f:
+    with open(f'./cache/response_v{args.version}_{args.version_name}/prefix.txt', 'w', encoding='utf-8') as f:
         f.write(prompt_prefix)
     
+    suffix = '答案：\n\t敘述正確或錯誤: ＿＿。\n\t證據: ＿＿。」'
     # choose candidates
     id_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
     for i in id_list:
+        # format the candidate
         candidate = claim_candidates[i]
-        # concat with the prompt
-        user_input = prompt_prefix + f'{candidate}'
-        user_input.replace("candidates", "候選證據文章").replace("claim", "敘述") #not working 
+        prompt = '敘述：' + f'{candidate["claim"]}\n' + '資料：\n'
+        for title in candidate['data']:
+            prompt += f'\t篇名：{title}\n\t內容：'
+            for sen in candidate['data'][title]:
+                prompt += f'[{sen}]{candidate["data"][title][sen]}'
+            prompt += '\n'
+        prompt.replace('\"', '').replace(':', '')
 
-        chatbot_response = get_response(user_input)
-        chatbot_response.update({'prompt' : user_input})
-        with open(f'./cache/response_v{version}/response{i}.json', 'w') as f:
+        # concat with the prompt
+        user_input = prompt_prefix + f'{prompt}'
+        chatbot_response = ''
+        try:
+            chatbot_response = get_response(user_input)
+            chatbot_response.update({'prompt' : user_input})
+            chatbot_response.update({'prompt_len' : len(user_input)})
+        except:
+            chatbot_response = 'token_error'
+        with open(f'./cache/response_v{args.version}_{args.version_name}/response{i}.json', 'w') as f:
             json.dump(chatbot_response, f, indent=2, ensure_ascii=False)
         print(f'finished generated response of training data_{i}')
 
-if __name__ == '__main__':
-    main()
+def parse_args() -> Namespace:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--preprocess_data",
+        type=Path,
+        help="Path to the train file.",
+        default="./cache/train_sentence_concat.json",
+        # required=True
+    )
+    parser.add_argument(
+        "--cache_dir",
+        type=Path,
+        help="Directory to the preprocessed caches.",
+        default="./cache/",
+    )
+    parser.add_argument(
+        "--version",
+        type=Path,
+        help="experiment version count",
+        required=True,
+    )
+    parser.add_argument(
+        "--version_name",
+        type=Path,
+        help="experiment title",
+        required=True,
+    )
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
