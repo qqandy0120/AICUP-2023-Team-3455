aa30e063a54fa0fdeb070c4c167fbf6447e51dd2
b09902141@ntu.edu.tw <yd3214813@gmail.com>
2023-05-22 02:40:06 +0800

    json convert add json to jsonl

--- json_convert.py
+++ json_convert.py
@@ -4,11 +4,25 @@ from pathlib import Path
 
 def main(args):
     Path('cache/').mkdir(parents=True, exist_ok=True)
+    if args.option == 0:
+        jsonl_to_json(args)
+    else:
+        json_to_jsonl(args)
+
+def jsonl_to_json(args):
     with open(args.jsonl_file, "r", encoding='utf-8') as f_in, open(args.json_file, "w", encoding='utf-8') as f_out:
         # read each line of JSONL file and append it to a list
         jsonl_list = [json.loads(line) for line in f_in]
         # write the entire list as a single JSON object to output file
         json.dump(jsonl_list, f_out, indent=2, ensure_ascii=False)
+
+def json_to_jsonl(args):
+    with open(args.json_file, "r", encoding='utf-8') as f_in, open(args.jsonl_file, "w", encoding='utf-8') as f_out:
+        # write the entire list as a single JSON object to output file
+        json_data = json.load(f_in)
+        for entry in json_data:
+            json.dump(entry, f_out, ensure_ascii=False)
+            f_out.write('\n')
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -21,6 +35,12 @@ def parse_args() -> Namespace:
         "--jsonl_file",
         type=Path,
         default="./data/public_train.jsonl",
+    )
+    parser.add_argument(
+        "--option",
+        type=int,
+        help='0: jsonl to json, 1:json to jsonl',
+        default=0
     )
     args = parser.parse_args()
     return args
