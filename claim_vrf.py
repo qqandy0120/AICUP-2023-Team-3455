766693c8a9be7538406a007a1e412a848d340947
b09902141@ntu.edu.tw <yd3214813@gmail.com>
2023-05-28 23:29:21 +0800

    claim vrf / simplified chinese / dynamic loading / limit ckpt

--- claim_vrf.py
+++ claim_vrf.py
@@ -6,10 +6,11 @@ import numpy as np
 import pandas as pd
 from pandarallel import pandarallel
 from tqdm.auto import tqdm
-
+import random
 import torch
 from sklearn.metrics import accuracy_score
 from torch.optim import AdamW
+from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from transformers import (
@@ -17,7 +18,6 @@ from transformers import (
     AutoTokenizer,
     get_scheduler,
 )
-
 from dataset import AicupTopkEvidenceBERTDataset
 from utils import (
     generate_evidence_to_wiki_pages_mapping,
@@ -28,8 +28,21 @@ from utils import (
     set_lr_scheduler,
 )
 from argparse import ArgumentParser, Namespace
-
+from collections import Counter
+import opencc
+import os 
 pandarallel.initialize(progress_bar=True, verbose=0, nb_workers=4)
+converter = opencc.OpenCC('t2s.json')  # 't2s.json' for Traditional to Simplified conversion
+
+def same_seeds(seed):
+    random.seed(seed) 
+    np.random.seed(seed)  
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed) 
+    torch.backends.cudnn.benchmark = False
+    torch.backends.cudnn.deterministic = True
 
 def run_evaluation(model: torch.nn.Module, dataloader: DataLoader, device):
     model.eval()
@@ -65,6 +78,10 @@ def run_predict(model: torch.nn.Module, test_dl: DataLoader, device) -> list:
         preds.extend(pred.tolist())
     return preds
 
+def shuffle_evidence(evidence):
+    random.shuffle(evidence)
+    return evidence
+
 def join_with_topk_evidence(
     df: pd.DataFrame,
     mapping: dict,
@@ -97,28 +114,54 @@ def join_with_topk_evidence(
 
     # format evidence column to List[List[Tuple[str, str, str, str]]]
     if "evidence" in df.columns:
-        df["evidence"] = df["evidence"].parallel_map(
+        df["evidence"] = df["evidence"].apply(
             lambda x: [[x]] if not isinstance(x[0], list) else [x]
             if not isinstance(x[0][0], list) else x)
 
     print(f"Extracting evidence_list for the {mode} mode ...")
     if mode == "eval":
         # extract evidence
-        df["evidence_list"] = df["predicted_evidence"].parallel_map(lambda x: [
+        df["evidence_list"] = df["predicted_evidence"].apply(lambda x: [
             mapping.get(evi_id, {}).get(str(evi_idx), "")
             for evi_id, evi_idx in x  # for each evidence list
         ][:topk] if isinstance(x, list) else [])
-        print(df["evidence_list"][:5])
+        df['evidence_list'] = df["evidence_list"].apply(lambda x:
+            [converter.convert(sent) for sent in x]
+        )
+        print(df["evidence_list"][:topk])
+        print( f'\nrunning {mode} mode\n')
     else:
-        # extract evidence
-        df["evidence_list"] = df["evidence"].parallel_map(lambda x: [
-            " ".join([  # join evidence
-                mapping.get(evi_id, {}).get(str(evi_idx), "")
-                for _, _, evi_id, evi_idx in evi_list
-            ]) if isinstance(evi_list, list) else ""
-            for evi_list in x  # for each evidence list
-        ][:1] if isinstance(x, list) else [])
+        # extract evidence [DEFAULT CODE]
+        # df["evidence_list"] = df["evidence"].parallel_map(lambda x: [
+        #     " ".join([  # join evidence
+        #         mapping.get(evi_id, {}).get(str(evi_idx), "")
+        #         for _, _, evi_id, evi_idx in evi_list
+        #     ]) if isinstance(evi_list, list) else ""
+        #     for evi_list in x  # for each evidence list
+        # ][:1] if isinstance(x, list) else [])
+        df["evidence_list"] = df["evidence"].apply(lambda x: [ [evi_id, evi_idx] 
+            for evi_list in x if isinstance(evi_list, list) 
+            for _, _, evi_id, evi_idx in evi_list if evi_id != None
+        ][:topk] if isinstance(x, list) else [])
 
+        df['concat_predicted_evidence'] = df["predicted_evidence"].apply( lambda x : random.sample(x, min(len(x), topk)))
+        df["evidence_list"] = df.apply(lambda row: (row['evidence_list'] + row['concat_predicted_evidence'])[:topk], axis=1)
+        df["evidence_list"] = df["evidence_list"].apply(shuffle_evidence)
+        df["evidence_list"] = df["evidence_list"].apply(lambda x: [
+            mapping.get(evi_id, {}).get(str(evi_idx), "")
+            for evi_id, evi_idx in x  # for each evidence list
+        ] if isinstance(x, list) else [])
+        
+        # convert to simplified chinese 
+        df['evidence_list'] = df["evidence_list"].apply(lambda x:
+            [converter.convert(sent) for sent in x]
+        )
+        # for debug
+        df['row_length'] = df["evidence_list"].apply(lambda row: len(row)) 
+        print(df['row_length']) 
+        
+        print(df["evidence_list"])
+        print(f'\nrunning {mode} mode\n')
     return df
 
 def main(args):
@@ -128,11 +171,10 @@ def main(args):
         "NOT ENOUGH INFO": 2,
     }
     ID2LABEL: Dict[int, str] = {v: k for k, v in LABEL2ID.items()}
-
     TRAIN_DATA = load_json(args.train_data)
     DEV_DATA = load_json(args.dev_data)
-    TRAIN_PKL_FILE = Path(args.train_pkl_file)
-    DEV_PKL_FILE = Path(args.dev_pkl_file)
+    # TRAIN_PKL_FILE = Path(args.train_pkl_file)
+    # DEV_PKL_FILE = Path(args.dev_pkl_file)
     #@title  { display-mode: "form" }
     MODEL_NAME = args.model_name  #@param {type:"string"}
     TRAIN_BATCH_SIZE = args.train_batch_size  #@param {type:"integer"}
@@ -144,9 +186,14 @@ def main(args):
     EVIDENCE_TOPK = args.top_n  #@param {type:"integer"}
     VALIDATION_STEP = args.validation_step  #@param {type:"integer"}
     OUTPUT_FILENAME = args.output_file
-    EXP_DIR = f"claim_verification/e{NUM_EPOCHS}_bs{TRAIN_BATCH_SIZE}_" + f"{LR}_top{EVIDENCE_TOPK}"
+    EXP_DIR = f"claim_verification/{args.exp_name}_e{NUM_EPOCHS}_bs{TRAIN_BATCH_SIZE}_" + f"{LR}_top{EVIDENCE_TOPK}" + f'_{MODEL_NAME}'
     LOG_DIR = "logs/" + EXP_DIR
     CKPT_DIR = "checkpoints/" + EXP_DIR
+    best_score = 0
+    BEST_CKPT = args.ckpt_name
+    validation_scores = []
+    # set random seed
+    same_seeds(SEED)
 
     print('[INFO] Preloading wiki database...')
     wiki_pages = jsonl_dir_to_df("data/wiki-pages")
@@ -161,28 +208,18 @@ def main(args):
         Path(CKPT_DIR).mkdir(parents=True)
 
     # Step 2. Concat claim and evidences
-    if not TRAIN_PKL_FILE.exists():
-        train_df = join_with_topk_evidence(
+    train_df = join_with_topk_evidence(
             pd.DataFrame(TRAIN_DATA),
             mapping,
             topk=EVIDENCE_TOPK,
         )
-        train_df.to_pickle(TRAIN_PKL_FILE, protocol=4)
-    else:
-        with open(TRAIN_PKL_FILE, "rb") as f:
-            train_df = pickle.load(f)
 
-    if not DEV_PKL_FILE.exists():
-        dev_df = join_with_topk_evidence(
+    dev_df = join_with_topk_evidence(
             pd.DataFrame(DEV_DATA),
             mapping,
             mode="eval",
             topk=EVIDENCE_TOPK,
         )
-        dev_df.to_pickle(DEV_PKL_FILE, protocol=4)
-    else:
-        with open(DEV_PKL_FILE, "rb") as f:
-            dev_df = pickle.load(f)
 
     # Step 3. Training
     torch.cuda.empty_cache()
@@ -212,28 +249,44 @@ def main(args):
     model.to(device)
     optimizer = AdamW(model.parameters(), lr=LR)
     num_training_steps = NUM_EPOCHS * len(train_dataloader)
-    lr_scheduler = set_lr_scheduler(optimizer, num_training_steps)
+    # lr_scheduler = set_lr_scheduler(optimizer, num_training_steps)
+    # scheduler with warmup
+    warmup_steps = (int)(num_training_steps/100)
+    lr_scheduler = LambdaLR(optimizer, lr_lambda=lambda epoch: min(epoch / warmup_steps, 1.0))
 
     if args.do_train == 1:
         writer = SummaryWriter(LOG_DIR)
         progress_bar = tqdm(range(num_training_steps))
         current_steps = 0
+        
+        # gradient accumulation
+        accumulation_steps = args.accumulation_step
+        total_loss = 0
+
         for epoch in range(NUM_EPOCHS):
             model.train()
             for batch in train_dataloader:
                 batch = {k: v.to(device) for k, v in batch.items()}
                 outputs = model(**batch)
                 loss = outputs.loss
+                
+                # gradient accumulation
+                loss = loss / accumulation_steps
+                total_loss += loss
                 loss.backward()
-                optimizer.step()
-                lr_scheduler.step()
 
-                optimizer.zero_grad()
-                progress_bar.update(1)
-                writer.add_scalar("training_loss", loss.item(), current_steps)
+                if (current_steps + 1) % accumulation_steps == 0:
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    lr_scheduler.step()
+                    progress_bar.update(accumulation_steps)
+                    writer.add_scalar("training_loss", total_loss, current_steps)
+                    total_loss = 0
+
                 y_pred = torch.argmax(outputs.logits, dim=1).tolist()
                 y_true = batch["labels"].tolist()
                 current_steps += 1
+
                 if current_steps % VALIDATION_STEP == 0 and current_steps > 0:
                     print("[INFO] Start validation")
                     val_results = run_evaluation(model, eval_dataloader, device)
@@ -241,51 +294,92 @@ def main(args):
                     for metric_name, metric_value in val_results.items():
                         print(f"{metric_name}: {metric_value}")
                         writer.add_scalar(f"{metric_name}", metric_value, current_steps)
+                    if best_score < val_results['val_acc']:
+                        best_score = val_results['val_acc']
+                        #val_acc=0.5453_model.13000.pt \
+                        BEST_CKPT = f"{val_results['val_acc']:.4f}_model.{current_steps}.pt"
+                    validation_scores.append(f"{val_results['val_acc']:.4f}")
                     save_checkpoint(
                         model,
                         CKPT_DIR,
                         current_steps,
-                        mark=f"val_acc={val_results['val_acc']:.4f}",
+                        mark=f"{val_results['val_acc']:.4f}",
                     )
+                    # save top K checkpoints
+                    validation_scores = sorted(validation_scores, reverse=True)[:args.save_checkpoints_limit]
+                    for file_name in os.listdir(CKPT_DIR):
+                        if file_name.endswith('.pt') and file_name.split('_')[0] not in validation_scores:
+                            os.remove(os.path.join(CKPT_DIR, file_name))
+
+            #TODO: reload dataset
+            train_df = join_with_topk_evidence(
+                pd.DataFrame(TRAIN_DATA),
+                mapping,
+                topk=EVIDENCE_TOPK,
+            )
+            train_dataset = AicupTopkEvidenceBERTDataset(
+                train_df,
+                tokenizer=tokenizer,
+                max_length=MAX_SEQ_LEN,
+            )
+            train_dataloader = DataLoader(
+                train_dataset,
+                shuffle=True,
+                batch_size=TRAIN_BATCH_SIZE,
+            )
         print("[INFO] Finished training!")
 
     # Step 4. Make your submission
     if args.do_validate == 1:
         TEST_DATA = load_json(args.test_data)
-        TEST_PKL_FILE = Path(args.test_pkl_file)
-
-        if not TEST_PKL_FILE.exists():  
-            print('[INFO] load test_df from {TEST_DATA}')
-            test_df = join_with_topk_evidence(
-                pd.DataFrame(TEST_DATA),
-                mapping,
-                mode="eval",
-                topk=EVIDENCE_TOPK,
-            )
-            test_df.to_pickle(TEST_PKL_FILE, protocol=4)
-        else:
-            with open(TEST_PKL_FILE, "rb") as f:
-                test_df = pickle.load(f)
-
+        # TEST_PKL_FILE = Path(args.test_pkl_file)
+        print(f'[INFO] load test_df from {TEST_DATA}')
+        test_df = join_with_topk_evidence(
+            pd.DataFrame(TEST_DATA),
+            mapping,
+            mode="eval",
+            topk=EVIDENCE_TOPK,
+        )
         test_dataset = AicupTopkEvidenceBERTDataset(
             test_df,
             tokenizer=tokenizer,
             max_length=MAX_SEQ_LEN,
         )
         test_dataloader = DataLoader(test_dataset, batch_size=TEST_BATCH_SIZE)
+        if args.do_ensemble == 0:
+            # ckpt_name = "val_acc=0.4259_model.750.pt"  #@param {type:"string"}
+            model = load_model(model, BEST_CKPT, CKPT_DIR)
+            predicted_label = run_predict(model, test_dataloader, device)
 
-        ckpt_name = "val_acc=0.4259_model.750.pt"  #@param {type:"string"}
-        model = load_model(model, ckpt_name, CKPT_DIR)
-        predicted_label = run_predict(model, test_dataloader, device)
-
-        predict_dataset = test_df.copy()
-        predict_dataset["predicted_label"] = list(map(ID2LABEL.get, predicted_label))
-        predict_dataset[["id", "predicted_label", "predicted_evidence"]].to_json(
-            OUTPUT_FILENAME,
-            orient="records",
-            lines=True,
-            force_ascii=False,
-        )
+            predict_dataset = test_df.copy()
+            predict_dataset["predicted_label"] = list(map(ID2LABEL.get, predicted_label))
+            predict_dataset[["id", "predicted_label", "predicted_evidence"]].to_json(
+                OUTPUT_FILENAME,
+                orient="records",
+                lines=True,
+                force_ascii=False,
+            )
+        elif args.do_ensemble == 1:
+            predict_label_list = []
+            predict_dataset = test_df.copy()
+            ckpt_list = sorted(os.listdir(CKPT_DIR), reverse=True)[:5]
+            for ckpt in ckpt_list:
+                print(f'loading {ckpt} to predict\n')
+                model = load_model(model, ckpt, CKPT_DIR)
+                predicted_label = run_predict(model, test_dataloader, device)
+                predict_label_list.append(predicted_label)
+                print(predicted_label[0])
+            # Transpose the lists using zip()
+            transposed_lists = zip(*predict_label_list)
+            # Find the most frequent elements at each position
+            ensemble_result = [Counter(column).most_common(1)[0][0] for column in transposed_lists]
+            predict_dataset["predicted_label"] = list(map(ID2LABEL.get, ensemble_result))
+            predict_dataset[["id", "predicted_label", "predicted_evidence"]].to_json(
+                OUTPUT_FILENAME,
+                orient="records",
+                lines=True,
+                force_ascii=False,
+            )
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
@@ -308,14 +402,14 @@ def parse_args() -> Namespace:
         "--train_pkl_file",
         type=Path,
         help="path to train pkl data",
-        required=True,
+        default='None',
     )
     # DEV_PKL_FILE = Path("data/dev_doc5sent5.pkl")
     parser.add_argument(
         "--dev_pkl_file",
         type=Path,
         help="path to dev pkl data",
-        required=True,
+        default='None'
     )
     # TEST_DATA = load_json("data/test_doc5sent5.jsonl")
     parser.add_argument(
@@ -329,7 +423,7 @@ def parse_args() -> Namespace:
         "--test_pkl_file",
         type=Path,
         help = 'path to evi retrieve test pkl file',
-        required=True,
+        default='123'
     )
     # OUTPUT_FILENAME = "submission.jsonl"
     parser.add_argument(
@@ -345,12 +439,36 @@ def parse_args() -> Namespace:
         help="pretrained model name",
         default="bert-base-chinese"
     )
+    parser.add_argument(
+        "--ckpt_name",
+        type=str,
+        help="ckpt name",
+        default=""
+    )
+    parser.add_argument(
+        "--exp_name",
+        type=str,
+        help="ckpt name",
+        required=True
+    )
     # NUM_EPOCHS = 20  #@param {type:"integer"}
     parser.add_argument(
         "--num_epoch",
         type=int,
         help="number epoch",
         default=20
+    )
+    parser.add_argument(
+        "--save_checkpoints_limit",
+        type=int,
+        help="save_checkpoints_limit",
+        default=10
+    )
+    parser.add_argument(
+        "--accumulation_step",
+        type=int,
+        help="gradient accumulation step",
+        default=8
     )
     # LR = 7e-5  #@param {type:"number"}
     parser.add_argument(
@@ -418,6 +536,12 @@ def parse_args() -> Namespace:
         type = int,
         help="whether to do training part",
         default=1
+    )
+    parser.add_argument(
+        "--do_ensemble",
+        type = int,
+        help="whether to do ensemble",
+        default=0
     )
     args = parser.parse_args()
     return args
