b652d6568396fce65edca118ddd3da2d720d3e3c
chacha <b08303028@ntu.edu.tw>
2023-05-31 18:19:15 +0800

    adding pr info

---
+++ cat_doc_rtv.py
@@ -0,0 +1,59 @@
+import typing 
+from typing import List, Dict
+import copy
+import jsonlines
+import json
+import os
+import pandas as pd
+from utils import load_json
+from doc_rtv import calculate_precision, calculate_recall
+from pathlib import Path
+
+def save_list_of_dicts_as_jsonl(data, filename):
+    with jsonlines.open(filename, 'w') as writer:
+        for item in data:
+            writer.write(item)
+
+MODE = 'test'
+
+data_path = f'data/all_{MODE}_doc_select_all.jsonl'
+with jsonlines.open(data_path, 'r') as f:
+    # datas = [line['predicted_pages'] for line in f]
+    datas = [line for line in f]
+
+# with open(data_path, 'r') as f:
+#     datas = json.load(f)
+
+# datas = [data['predicted_pages'] for data in datas]
+
+
+es_path = Path(f'data/all_es_{MODE}_token_10.txt')
+with open(es_path, 'r') as f:
+    lines = f.readlines()
+    es_tokens: List[List[str]] = [line.replace('\n', '').split(' ') for line in lines]
+
+
+MIN_DOCN, MAX_DOCN = 0, 9
+MIN_ESN, MAX_ESN = 0, 9
+
+for doc_n in range(MIN_DOCN, MAX_DOCN+1):
+    for es_n in range(MIN_ESN, MAX_ESN+1):
+        # print('-------')
+        # print(datas[0]['predicted_pages'])
+        # print('-------')
+        select_tokens = []
+        for line in datas:
+            select_tokens.append(line['predicted_pages'])
+        concat_tokens = []
+        for i in range(len(select_tokens)):
+            concat_tokens.append(list(set(select_tokens[i][:doc_n]+es_tokens[i][:es_n])))
+
+        out_dir = Path(f'cache/all_{MODE}_doc{doc_n}')
+        out_dir.mkdir(parents=True, exist_ok=True) 
+
+        result = copy.deepcopy(datas)
+        for i, line in enumerate(result):
+            line['predicted_pages'] = concat_tokens[i]
+
+        print(os.path.join(out_dir, f'es{es_n}.jsonl'))
+        save_list_of_dicts_as_jsonl(result, os.path.join(out_dir, f'es{es_n}.jsonl'))
---
+++ get_pr.py
@@ -0,0 +1,105 @@
+# built-in libs
+import json
+import pickle
+import re
+from dataclasses import dataclass
+from pathlib import Path
+from typing import Dict, List, Set, Tuple, Union
+
+# 3rd party libs
+import hanlp
+import opencc
+import pandas as pd
+import wikipedia
+from hanlp.components.pipeline import Pipeline
+from pandarallel import pandarallel
+
+# our own libs
+from utils import load_json
+
+def calculate_precision(
+    data,
+    predictions: pd.Series,
+) -> None:
+    precision = 0
+    count = 0
+
+    for i, d in enumerate(data):
+        if d["label"] == "NOT ENOUGH INFO":
+            continue
+
+        # Extract all ground truth of titles of the wikipedia pages
+        # evidence[2] refers to the title of the wikipedia page
+        gt_pages = set([
+            evidence[2]
+            for evidence_set in d["evidence"]
+            for evidence in evidence_set
+        ])
+
+        predicted_pages = predictions.iloc[i]
+        hits = predicted_pages.intersection(gt_pages)
+        if len(predicted_pages) != 0:
+            precision += len(hits) / len(predicted_pages)
+
+        count += 1
+
+    # Macro precision
+    print(f"Precision: {precision / count}")
+
+    return precision / count
+def calculate_recall(
+    data,
+    predictions: pd.Series,
+) -> None:
+    recall = 0
+    count = 0
+
+    for i, d in enumerate(data):
+        if d["label"] == "NOT ENOUGH INFO":
+            continue
+
+        gt_pages = set([
+            evidence[2]
+            for evidence_set in d["evidence"]
+            for evidence in evidence_set
+        ])
+        predicted_pages = predictions.iloc[i]
+        hits = predicted_pages.intersection(gt_pages)
+        recall += len(hits) / len(gt_pages)
+        count += 1
+
+    print(f"Recall: {recall / count}")
+
+    return recall / count
+
+TRAIN_DATA = load_json("data/all_train_data.jsonl")
+
+result = {}
+strong_results = {}
+MIN_DOCN, MAX_DOCN = 0, 9
+MIN_ESN, MAX_ESN = 0, 9
+
+for doc_n in range(MIN_DOCN, MAX_DOCN+1):
+    result[f'doc_{doc_n}'] = {}
+    for es_n in range(MIN_ESN, MAX_ESN+1):
+        with open(f"data/all/all_train_doc{doc_n}/es{es_n}.jsonl", "r", encoding="utf8") as f:
+                predicted_results = pd.Series([
+                    set(json.loads(line)["predicted_pages"])
+                    for line in f
+                ])
+                p = calculate_precision(TRAIN_DATA, predicted_results)
+                r = calculate_recall(TRAIN_DATA, predicted_results)
+
+                result[f'doc_{doc_n}'][f'es_{es_n}'] = {
+                    'precision': p,
+                    'recall': r
+                }
+
+                if p > 0.18 and r > 0.91:
+                    strong_results.update({f'doc{doc_n}_es{es_n}': {'precision': p, 'recall': r}})
+
+with open('pr_results.json', 'w') as f:
+    json.dump(result, f, indent=2)
+strong_results = dict(sorted(strong_results.items(), key=lambda x: x[1]['recall'], reverse=True))
+with open('strong_pr_results.json', 'w') as f:
+    json.dump(strong_results, f, indent=2)
