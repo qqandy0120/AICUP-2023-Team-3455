d603eea83ad47e72930b00ed0fa0ff73e937a2fc
b09902141@ntu.edu.tw <yd3214813@gmail.com>
2023-06-01 00:20:55 +0800

    train claim update

--- train_claim.sh
+++ train_claim.sh
@@ -1,24 +1,21 @@
-#!/usr/bin/env bash
-
-python3 stc_rtv.py \
---train_data data/all_train_data.jsonl \
---test_data data/all_test_data.jsonl \
---train_doc_data data/all/all_train_doc9/es9.jsonl \
---test_doc_data data/all/all_test_doc9/es9.jsonl \
---exp_name test_new_data \
+CUDA_VISIBLE_DEVICES="0,1" python3 claim_vrf.py \
+--train_data data/all_data_len256_lr7_7_top5/train_model.60000.pt_9.jsonl  \
+--test_data data/all_data_len256_lr7_7_top5/test_model.60000.pt_9.jsonl \
+--dev_data data/all_data_len256_lr7_7_top5/dev_model.60000.pt_9.jsonl \
+--output_file submission_0601_macbert_large_Train_step3_Top9.jsonl \
 --model_name hfl/chinese-macbert-large \
---num_epoch 5 \
---train_batch_size 6 \
---test_batch_size 6 \
---validation_step 2000 \
---top_n 5 \
---neg_ratio 0.05 \
---lr 3.7e-5 \
+--exp_name all_data_best_step2_train_top9 \
+--test_batch_size 8 \
+--train_batch_size 8 \
+--validation_step 1000 \
+--seed 3513 \
+--num_epoch 125 \
 --max_seq_len 512 \
---test_size 0.035 \
---do_t2s 1 \
---do_dynamic_load_neg 1 \
---do_single_evi_train 1 \
---do_concat_page_name_train 1 \
---do_test 0 \
+--lr 8.3e-5 \
+--accumulation_step 32 \
+--do_train 1 \
 --do_validate 0 \
+--do_test 0 \
+--do_ensemble 1 \
+--do_ensemble_topk 5 \
+--save_checkpoints_limit 25 \
