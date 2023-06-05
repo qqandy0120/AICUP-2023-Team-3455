322926c56d553a17edfd788db8619b9b7e7fa38a
chacha <b08303028@ntu.edu.tw>
2023-06-06 01:02:26 +0800

    final search

--- README.md
+++ README.md
@@ -15,3 +15,12 @@ pip install -r requirements.in
 ```bash
 bash download.sh
 ```
+
+## Built up elasticsearch
+check [search/main](search/main.ipynb), the ipynb have step by step tutorial. <br>
+If you successfully produce all_es_train_token_10.txt and all_es_test_token_10.txt, then you have done this step.
+
+## Concat index-based document retrieval with elasticsearch(BM-25) document retrieval.
+```bash
+python cat_doc_rtv.py
+```
--- cat_doc_rtv.py
+++ cat_doc_rtv.py
@@ -14,46 +14,47 @@ def save_list_of_dicts_as_jsonl(data, filename):
         for item in data:
             writer.write(item)
 
-MODE = 'test'
+MODES = ['train', 'test']
 
-data_path = f'data/all_{MODE}_doc_select_all.jsonl'
-with jsonlines.open(data_path, 'r') as f:
-    # datas = [line['predicted_pages'] for line in f]
-    datas = [line for line in f]
+for mode in MODES:
+    data_path = f'data/all_{mode}_doc_select_all.jsonl'
+    with jsonlines.open(data_path, 'r') as f:
+        # datas = [line['predicted_pages'] for line in f]
+        datas = [line for line in f]
 
-# with open(data_path, 'r') as f:
-#     datas = json.load(f)
+    # with open(data_path, 'r') as f:
+    #     datas = json.load(f)
 
-# datas = [data['predicted_pages'] for data in datas]
-
-
-es_path = Path(f'data/all_es_{MODE}_token_10.txt')
-with open(es_path, 'r') as f:
-    lines = f.readlines()
-    es_tokens: List[List[str]] = [line.replace('\n', '').split(' ') for line in lines]
+    # datas = [data['predicted_pages'] for data in datas]
 
 
-MIN_DOCN, MAX_DOCN = 0, 9
-MIN_ESN, MAX_ESN = 0, 9
+    es_path = Path(f'data/all_es_{mode}_token_10.txt')
+    with open(es_path, 'r') as f:
+        lines = f.readlines()
+        es_tokens: List[List[str]] = [line.replace('\n', '').split(' ') for line in lines]
 
-for doc_n in range(MIN_DOCN, MAX_DOCN+1):
-    for es_n in range(MIN_ESN, MAX_ESN+1):
-        # print('-------')
-        # print(datas[0]['predicted_pages'])
-        # print('-------')
-        select_tokens = []
-        for line in datas:
-            select_tokens.append(line['predicted_pages'])
-        concat_tokens = []
-        for i in range(len(select_tokens)):
-            concat_tokens.append(list(set(select_tokens[i][:doc_n]+es_tokens[i][:es_n])))
 
-        out_dir = Path(f'cache/all_{MODE}_doc{doc_n}')
-        out_dir.mkdir(parents=True, exist_ok=True) 
+    MIN_DOCN, MAX_DOCN = 0, 9
+    MIN_ESN, MAX_ESN = 0, 9
 
-        result = copy.deepcopy(datas)
-        for i, line in enumerate(result):
-            line['predicted_pages'] = concat_tokens[i]
+    for doc_n in range(MIN_DOCN, MAX_DOCN+1):
+        for es_n in range(MIN_ESN, MAX_ESN+1):
+            # print('-------')
+            # print(datas[0]['predicted_pages'])
+            # print('-------')
+            select_tokens = []
+            for line in datas:
+                select_tokens.append(line['predicted_pages'])
+            concat_tokens = []
+            for i in range(len(select_tokens)):
+                concat_tokens.append(list(set(select_tokens[i][:doc_n]+es_tokens[i][:es_n])))
 
-        print(os.path.join(out_dir, f'es{es_n}.jsonl'))
-        save_list_of_dicts_as_jsonl(result, os.path.join(out_dir, f'es{es_n}.jsonl'))
+            out_dir = Path(f'cache/all_{mode}_doc{doc_n}')
+            out_dir.mkdir(parents=True, exist_ok=True) 
+
+            result = copy.deepcopy(datas)
+            for i, line in enumerate(result):
+                line['predicted_pages'] = concat_tokens[i]
+
+            print(os.path.join(out_dir, f'es{es_n}.jsonl'))
+            save_list_of_dicts_as_jsonl(result, os.path.join(out_dir, f'es{es_n}.jsonl'))
